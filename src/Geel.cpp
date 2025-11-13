b2e522a769ebb06de19dda2670e63ff17ff11e38
Nikita <zzbz813@gmail.com>
2025-11-13 21:46:51 +0300

    Modification

--- src/Geel.cpp
+++ src/Geel.cpp
@@ -6,24 +6,25 @@ int main(void)
     try
     {
         auto window = std::make_unique<Window>(1280, 720, "Geel Engine");
-
-        if (!window->gladInit()) {
+        if (!window->gladInit())
+        {
             throw std::runtime_error("Failed to initialize GLAD");
-            return -1;
         }
-        
+
         glViewport(0, 0, window->getWidth(), window->getHeight());
 
-        while (!window->shouldClose()) {
+        while (!window->shouldClose())
+        {
             glClear(GL_COLOR_BUFFER_BIT);
             glfwSwapBuffers(window->getWindow());
             glfwPollEvents();
         }
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << "Error: " << e.what() << '\n';
+        return -1;
     }
-    
+
     return 0;
 }
